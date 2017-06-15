6575d7e054494e8a2848ae9f7ad97eb093698c57
Laurent Claustre <claustre@esrf.fr>
2017-06-15 08:04:19 +0200

    This is working fine under redhate5

--- python/__init__.py
+++ python/__init__.py
@@ -19,23 +19,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <http://www.gnu.org/licenses/>.
 ############################################################################
-from Lima import module_helper
-
-mod_path = __path__
-depends_on = 'Core'
-has_dependent = True
-
-cleanup_data = module_helper.load_prepare(mod_path, depends_on, has_dependent)
-
 from Lima import Core
 
-cleanup_data = module_helper.load_dep_cleanup(cleanup_data)
-
-from limaespia import *
-globals().update(Espia.__dict__)
-
-module_helper.load_cleanup(cleanup_data)
-
-del mod_path, depends_on, has_dependent, cleanup_data
-del module_helper
+from limaespia import Espia as _E
+globals().update(_E.__dict__)
 
